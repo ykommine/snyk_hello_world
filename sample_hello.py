dd8e6e505d901077c9528512da6b3b7eddbc84b8
ykommine <87748818+ykommine@users.noreply.github.com>
2024-06-18 15:39:03 +0530

    Add files via upload

---
+++ sample_hello.py
@@ -0,0 +1,14 @@
+
+print('Hello World')
+
+a = 12
+b = 5
+
+for i in range(a):
+    if a-(a-1) <= 5:
+        print(a, b)
+        print(a-1)
+        a = a-1
+    else:
+        print('not sat')
+        a = a-1
